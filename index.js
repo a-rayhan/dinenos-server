4b299a153ba150e6c93b644e7049cb4a6c684d96
rayhan <aburayhandh@gmail.com>
2023-11-08 22:53:08 +0600

    create db collections

--- index.js
+++ index.js
@@ -48,3 +48,33 @@ const verifyToken = async (req, res, next) => {
 
 
 }
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        const dinenosCollection = client.db("dinenosDb").collection("allFoodItems");
+        const purchaseCollection = client.db("dinenosDb").collection("purchaseItems");
+        const blogCollection = client.db("dinenosDb").collection("blog");
+
+        
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Dinenos is running')
+})
+
+app.listen(port, () => {
+    console.log(`Dinenos is running at PORT: ${port}`);
+})
